e4abf5159260506f11aa9f3710581578ded32678
RongY <yang.ariel@hotmail.com>
2020-11-17 13:53:52 -0500

    use this.value update counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   // --- our code goes here ---
   console.log("I am ready");
   $("#tweet-text").keydown(function() {
-    $("output.counter").text(140 - $("#tweet-text").val().length);
+    $("output.counter").text(140 -this.value.length);
   })
 });
