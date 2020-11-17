e7f3cc0e0ca223c06ced60e71fcabc3b5e6873c9
RongY <yang.ariel@hotmail.com>
2020-11-17 13:56:42 -0500

    text counter turn red  over 140

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,5 +3,8 @@ $(document).ready(function() {
   console.log("I am ready");
   $("#tweet-text").keydown(function() {
     $("output.counter").text(140 -this.value.length);
+    if (140 - this.value.length < 0) {
+      $("output.counter").css('color', 'red');
+    }
   })
 });
