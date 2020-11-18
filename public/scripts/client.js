cec483c3044240194b7769eb87399151e368cbe0
RongY <yang.ariel@hotmail.com>
2020-11-18 15:11:36 -0500

    dynamically create new Tweet Element

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,51 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const tweetDate = 
+{
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png"
+    ,
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
 
+const createTweetElement = (data) => {
+  return  ` <article class = 'tweet'>
+          <header>
+            <div class = 'name'>
+              <image src="${data.user.avatars}"/>
+              <span>${data.user.name}</span>
+            </div>
+            <div class="account">
+              <span>${data.user.handle}</span>
+            </div>
+          </header>
+          <p>
+          ${data.content.text}
+          </p>
+          <footer>
+            <div class = 'date'>
+            ${data.created_at}
+            </div>
+            <div class = "action">
+               icons
+            </div>
+          </footer>
+        </article>`
+}
+const $tweet = createTweetElement(tweetDate);
+
+
+
+$(document).ready(function() {
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('.tweet-container').append($tweet);
+
+});
