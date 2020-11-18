3000710c76feda25f11b684dc6413e7586d3e93f
RongYangAriel <yang.ariel@hotmail.com>
2020-11-17 21:31:28 -0500

    fix bug

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
   // --- our code goes here ---
   console.log("I am ready");
-  $("#tweet-text").keydown(function() {
+  $("#tweet-text").keyup(function() {
     $("output.counter").text(140 -this.value.length);
+    console.log('fired');
     if (140 - this.value.length < 0) {
       $("output.counter").css('color', 'red');
     }
